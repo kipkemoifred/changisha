5560a6dc7f00df7f6a3c0f3a81840e7497953f5a
kipkemoifred <44499281+kipkemoifred@users.noreply.github.com>
2021-05-26 15:04:00 +0100

    first changisha commit

---
+++ changisha/src/main/java/com/example/changisha/ChangishaApplication.java
@@ -0,0 +1,15 @@
+package com.example.changisha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangishaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangishaApplication.class, args);
+		System.out.println("hii");
+
+	}
+
+}
